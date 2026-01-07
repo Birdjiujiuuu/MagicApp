db8ee4719b6a710932b4891c5f4f9c9e66a71d41
Birdjiujiuuu <h2097571086@outlook.com>
2026-01-07 22:58:45 +0800

    修复bugs

--- MagicApp/Pages/NoticePage.xaml.cs
+++ MagicApp/Pages/NoticePage.xaml.cs
@@ -1,19 +1,7 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using Microsoft.Windows.AppNotifications;
 using Microsoft.Windows.AppNotifications.Builder;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -28,6 +16,12 @@ namespace MagicApp.Pages
         public NoticePage()
         {
             InitializeComponent();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            ContentTitle.Text = string.Empty;
+            ContentContent.Text = string.Empty;
         }
 
         private void Send_Click(object sender, RoutedEventArgs e)
