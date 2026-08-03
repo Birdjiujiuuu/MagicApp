a3f4ce74fd4aae3d9096eee4883d6cba7d4e016f
Birdjiujiuuu <h2097571086@outlook.com>
2026-08-03 10:08:47 +0800

    修复已知问题

--- MagicApp/Pages/TimestampPage.xaml.cs
+++ MagicApp/Pages/TimestampPage.xaml.cs
@@ -1,7 +1,9 @@
+using MagicApp.Helpers;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Navigation;
 using System;
+using Windows.ApplicationModel.DataTransfer;
 
 namespace MagicApp.Pages
 {
@@ -13,9 +15,16 @@ namespace MagicApp.Pages
         {
             this.InitializeComponent();
 
+            // 填充秒下拉列表
+            for (int i = 0; i < 60; i++)
+            {
+                SecondsComboBox.Items.Add(i.ToString("D2"));
+            }
+
             // 初始化日期时间选择器为当前时间
             DatePickerInput.Date = DateTimeOffset.Now;
             TimePickerInput.Time = DateTimeOffset.Now.TimeOfDay;
+            SecondsComboBox.SelectedItem = DateTime.Now.Second.ToString("D2");            
 
             // 启动实时时钟
             StartClock();
@@ -36,7 +45,7 @@ namespace MagicApp.Pages
             CurrentTimeText.Text = now.ToString("yyyy-MM-dd HH:mm:ss");
 
             // 获取 UTC 时间戳
-            long unixSeconds = ((DateTimeOffset)now).ToUnixTimeSeconds();
+            long unixSeconds = FormatHelper.DateTimeToUnixTimeStamp(now);
             CurrentTimestampText.Text = unixSeconds.ToString();
         }
 
@@ -46,10 +55,14 @@ namespace MagicApp.Pages
             try
             {
                 DateTime selectedDate = DatePickerInput.Date.DateTime;
-                TimeSpan selectedTime = TimePickerInput.Time;
-                DateTime localDateTime = selectedDate.Date + selectedTime;
+                int hour = TimePickerInput.Time.Hours;
+                int minute = TimePickerInput.Time.Minutes;
+                string secondStr = SecondsComboBox.SelectedItem?.ToString() ?? "00";
+                int second = int.Parse(secondStr);
 
-                long unixSeconds = ((DateTimeOffset)localDateTime).ToUnixTimeSeconds();
+                DateTime localDateTime = new DateTime(selectedDate.Year, selectedDate.Month, selectedDate.Day,hour, minute, second);
+
+                long unixSeconds = FormatHelper.DateTimeToUnixTimeStamp(localDateTime);
                 ConvertedTimestampResult.Text = unixSeconds.ToString();
             }
             catch
@@ -58,8 +71,8 @@ namespace MagicApp.Pages
             }
         }
 
-        // 时间戳转换日期时间 
-        private void OnConvertTimestampToDateTime(object sender, RoutedEventArgs e)
+        // 时间戳转换日期时间
+        private void TimestampInput_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
         {
             try
             {
@@ -69,14 +82,39 @@ namespace MagicApp.Pages
                     return;
                 }
 
-                DateTimeOffset utcDateTime = DateTimeOffset.FromUnixTimeSeconds(unixSeconds);
-                DateTime localDateTime = utcDateTime.LocalDateTime;
+                DateTime localDateTime = FormatHelper.UnixTimeStampToDateTime(unixSeconds);
                 ConvertedDateTimeResult.Text = localDateTime.ToString("yyyy-MM-dd HH:mm:ss");
             }
             catch (ArgumentOutOfRangeException)
             {
                 ConvertedDateTimeResult.Text = "时间戳超出有效范围";
             }
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            CopyTimestampResultButton.Action = async () =>
+            {
+                if (string.IsNullOrWhiteSpace(ConvertedTimestampResult.Text))
+                {
+                    return false;
+                }
+                else
+                {
+                    try
+                    {
+                        var package = new DataPackage();
+                        package.SetText(ConvertedTimestampResult.Text);
+                        Clipboard.SetContent(package);
+                        return true;
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                }
+                
+            };
         }
 
         // 页面离开时停止定时器，释放资源
